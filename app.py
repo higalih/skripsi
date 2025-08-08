4a783347abf86a82441cd47d567bff592c7c1c65
Galih <hi9alih@gmail.com>
2025-08-08 11:06:03 +0700

    add more log to route

--- app.py
+++ app.py
@@ -216,33 +216,6 @@ def reset():
     return redirect(url_for("index"))
 
 
-#
-# @app.route("/kaggle_image/<int:recipe_id>")
-# def serve_image(recipe_id):
-# temp_path = f"/tmp/{recipe_id}.jpg"
-# if not os.path.exists(temp_path):
-# if kaggle_api:
-# try:
-# kaggle_api.dataset_download_file(
-# "elisaxxygao/foodrecsysv1",
-# f"raw-data-images/raw-data-images/{recipe_id}.jpg",
-# path="/tmp",
-# quiet=True,
-# force=True,
-# )
-# except Exception as e:
-# print(f"Could not fetch image: {e}")
-# return "Image unavailable", 404
-# else:
-# return "Image unavailable", 404
-# try:
-# return send_file(temp_path, mimetype="image/jpeg")
-# except Exception as e:
-# print(f"Could not send image: {e}")
-# return "Image unavailable", 404
-#
-
-
 @app.route("/kaggle_image/<int:recipe_id>")
 def serve_image(recipe_id):
     temp_path = f"/tmp/{recipe_id}.jpg"
