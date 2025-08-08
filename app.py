01b5fa616210e1de1052671a1e59f441a23c1a51
Galih <hi9alih@gmail.com>
2025-08-08 11:15:27 +0700

    comment out app.run

--- app.py
+++ app.py
@@ -248,5 +248,5 @@ def serve_image(recipe_id):
         return "Image unavailable", 404
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+# app.run(host="0.0.0.0", port=5000, debug=True)
